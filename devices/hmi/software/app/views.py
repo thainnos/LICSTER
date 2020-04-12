03a046318347b2e10b2250dc4321b24bdc31671e
toni <klemm494@gmail.com>
2020-04-12 10:15:41 +0200

    Added setup file

--- devices/hmi/software/app/views.py
+++ devices/hmi/software/app/views.py
@@ -167,26 +167,6 @@ def login():
     :return: The login.html view
     Note: Has no functionality yet
     """
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
-
-        flash(error)
 
     return render_template('login.html')
 
