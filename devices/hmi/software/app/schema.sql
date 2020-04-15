d54a188d2c1e91bcd6b3b5c32976d0f714d2dd5d
toni <klemm494@gmail.com>
2020-04-15 19:59:34 +0200

    Added sql schema with table users

---
+++ devices/hmi/software/app/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  user_role TEXT NOT NULL,
+  first_login INTEGER NOT NULL
+);
