4b54714996a95f8013ec05d33ee96ef3c21e79d9
Antonio Klemm <klemm494@gmail.com>
2020-09-08 22:21:00 +0200

    Reduced complexity of login method. Removed the development method nonlocallogin.

--- devices/hmi/software/app/auths.py
+++ devices/hmi/software/app/auths.py
@@ -49,150 +49,88 @@ def load_logged_in_user():
         ).fetchone()
 
 
+def save_ip_address_if_not_saved(userId):
+    db = get_db()
+    ipaddr = db.execute(
+        'SELECT * FROM ipaddr WHERE userid = ?', (userId,)
+    ).fetchall()
+    ipSet = False
+    if ipaddr:
+        # The user already has at least one IP Adress in the database
+        for ip in ipaddr:
+            if ip['ipaddress'] == request.remote_addr:
+                ipSet = True
+                if ipSet:
+                    break
+
+    if not ipSet:
+        # The current Ip adress is not in the database
+        db.execute(
+            'INSERT INTO ipaddr (userid, ipaddress) VALUES (?, ?)',
+            (session['user_id'], request.remote_addr)
+        )
+        db.commit()
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 @logout_required
+# flake8: noqa: C901
 def login():
-    ''' Login view. Returns a view for the local HMI
-    and another for any remote connection.
+    isHmi = False
+    if request.remote_addr == "127.0.0.1":
+        isHmi = True
+    # Formen generieren
+    if isHmi:
+        form = HmiLoginForm()
+    else:
+        form = LoginForm()    
 
-    :return:
-    Before validation: login.html
-    After validation: index.html or admin dashboard
-    :rtype: HTML / redirect
-    '''
-    form = LoginForm()
-    hmiForm = HmiLoginForm()
-    if hmiForm.validate_on_submit() and request.remote_addr == "127.0.0.1":
-        db = get_db()
-        username = "hmilocal"
-        password = hmiForm.password.data
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-        if check_password_hash(user['password'], password):
-            session.clear()
-            session['user_id'] = user['id']
-            ipaddr = db.execute(
-                'SELECT * FROM ipaddr WHERE userid = ?', (session['user_id'],)
-            ).fetchall()
-            if ipaddr:
-                # The user already has an Ip Adress in the database
-                pass
-            else:
-                # The current Ip adress is not in the database
-                db.execute(
-                    'INSERT INTO ipaddr (userid, ipaddress) VALUES (?, ?)',
-                    (session['user_id'], request.remote_addr)
-                )
-                db.commit()
-            session['user_role'] = user['user_role']
-            return redirect(url_for('views.index'))
+    # Form validieren
     if form.validate_on_submit():
-        username = form.username.data
+        # get username, password from form
+        # get user with db query
+        # check if user exists in db and password hash matches
+        if isHmi:
+            username = 'hmilocal'
+        else:
+            username = form.username.data
         password = form.password.data
         db = get_db()
-        error = None
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
+        error = None
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
         if error is None:
+            # User exists in db and password hash matches
+            # User ID und Rolle der Session hinzufügen
             session.clear()
             session['user_id'] = user['id']
-            ipaddr = db.execute(
-                'SELECT * FROM ipaddr WHERE userid = ?', (session['user_id'],)
-            ).fetchall()
-            ipnotset = True
-            if ipaddr:
-                # The user already has an Ip Adress in the database
-                for ip in ipaddr:
-                    if ip['ipaddress'] == request.remote_addr:
-                        ipnotset = False
-            if ipnotset:
-                # The current Ip adress is not in the database
-                db.execute(
-                    'INSERT INTO ipaddr (userid, ipaddress) VALUES (?, ?)',
-                    (session['user_id'], request.remote_addr)
-                )
-                db.commit()
-
             session['user_role'] = user['user_role']
+
+            save_ip_address_if_not_saved(user['id'])
+
+            if isHmi:
+                return redirect(url_for('views.index'))
+            # Nur für nicht HMI User:
+            # Falls nötig zum Passwort setzen weiterleiten
             if user['first_login'] == 1:
                 return redirect(url_for('auths.set_password'))
+            # Falls Admin zum Dashboard weiterleiten
             if session['user_role'] == 'admin':
-                return redirect(url_for('admins.dashboard'))
+                return redirect(url_for('admins.dashboard'))          
             return redirect(url_for('views.index'))
 
-        flash(error)
-
-    if request.remote_addr == "127.0.0.1":
-        return render_template('loginhmi.html', form=hmiForm)
+    # Template rendern
+    if isHmi:
+        return render_template('loginhmi.html', form=form)
     else:
         return render_template('login.html', form=form)
-
-# DELETE ME!!!
-# Seriously, don't use me!!
-
-
-@auth.route('/loginnonlocal', methods=['GET', 'POST'])
-@logout_required
-def login_local():
-    ''' Remote login view. Returns the remote view on the local machine.
-    :return:
-    Before validation the login.html view,
-    after validation the index.html/admin dashboard
-    :rtype: HTML / redirect
-    '''
-    form = LoginForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            ipaddr = db.execute(
-                'SELECT * FROM ipaddr WHERE userid = ?', (session['user_id'],)
-            ).fetchall()
-            ipnotset = True
-            if ipaddr:
-                # The user already has an Ip Adress in the database
-                for ip in ipaddr:
-                    if ip['ipaddress'] == request.remote_addr:
-                        ipnotset = False
-            if ipnotset:
-                # The current Ip adress is not in the database
-                db.execute(
-                    'INSERT INTO ipaddr (userid, ipaddress) VALUES (?, ?)',
-                    (session['user_id'], request.remote_addr)
-                )
-                db.commit()
-
-            if user['first_login'] == 1:
-                return redirect(url_for('auths.set_password'))
-            session['user_role'] = user['user_role']
-            if session['user_role'] == 'admin':
-                return redirect(url_for('admins.dashboard'))
-            return redirect(url_for('views.index'))
-
-        flash(error)
-
-    return render_template('login.html', form=form)
 
 
 @auth.route('/logout')
