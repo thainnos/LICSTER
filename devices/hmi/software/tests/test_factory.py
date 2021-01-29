22d93537467e5e3a63f02dd36772f37b25b7c51b
sunnyprogram <klemm494@gmail.com>
2021-01-29 20:21:32 +0100

    Added a test for the page_not_found method.

--- devices/hmi/software/tests/test_factory.py
+++ devices/hmi/software/tests/test_factory.py
@@ -4,3 +4,9 @@ from app import create_app
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
+
+
+def test_page_not_found(client):
+    with client:
+        response = client.get("/thispagedoesntexist")
+        assert response.status_code == 404
