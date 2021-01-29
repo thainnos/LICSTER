5ea7aae8fd560c5c2262079553928048a13dd6c2
sunnyprogram <klemm494@gmail.com>
2021-01-29 20:24:47 +0100

    Added a test to check if the correct response headers are used.

--- devices/hmi/software/tests/test_factory.py
+++ devices/hmi/software/tests/test_factory.py
@@ -10,3 +10,11 @@ def test_page_not_found(client):
     with client:
         response = client.get("/thispagedoesntexist")
         assert response.status_code == 404
+
+
+def test_request_headers(client):
+    with client:
+        response = client.get("/")
+        assert response.headers["X-Frame-Options"] == "DENY"
+        assert response.headers["X-Content-Type-Options"] == "nosniff"
+        assert response.headers["Server"] == "gunicorn"
